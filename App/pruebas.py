60b5a7ff62050231bc18089dd974e7b58df658d3
ErichGiusseppe <erichgiusseppesotop@gmail.com>
2020-12-08 20:34:42 -0500

    .

--- App/pruebas.py
+++ App/pruebas.py
@@ -1,9 +1,21 @@
-x=[1,2,3,4,5,6]
-def hola(lista):
-    jk(lista)
-    print(lista)
-def jk(lista):
-    lista.append(9)
-    print(lista)
-    return 0
-hola(x)
+import config as cf
+from DISClib.ADT import indexminpq as pq
+
+def greater(key1, key2):
+    key2=key2["key"]
+    if key1 == key2:
+        return 0
+    elif key1 > key2:
+        return -1
+    else:
+        return 1
+minpq = pq.newIndexMinPQ(greater)
+
+pq.insert(minpq, "5",5)
+pq.insert(minpq, "23",23)
+pq.insert(minpq, "31",31)
+pq.insert(minpq, "15",15)
+
+print(pq.delMin(minpq))
+print(pq.delMin(minpq))
+print(pq.delMin(minpq))
